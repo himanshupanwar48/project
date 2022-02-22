3c3eb5bbf0d292b860fe0f5f5d29a9af55d72e0b
Himanshu panwar <himanshupanwar480@gmail.com>
2022-02-22 20:14:30 +0530

    Project committed by himanshu

---
+++ project/src/main/java/himanshu/NamePrint.java
@@ -0,0 +1,12 @@
+package himanshu;
+
+public class NamePrint {
+
+	public static void main(String[] args) {
+		String s1="himanshu";
+        String s2="panwar";
+        System.out.println("enter the name:");
+        System.out.println("hello"+s1.concat(s2));
+	}
+
+}
